40600861a971b325897de89820d8d918a37c5c0a
olegzheleztsov <olegzheleztsov@gmail.com>
2025-09-14 22:38:37 +0300

    Ts sample

--- dist/index.js
+++ dist/index.js
@@ -27276,6 +27276,23 @@ async function run() {
         coreExports.debug(new Date().toTimeString());
         // Set outputs for other workflow steps to use
         coreExports.setOutput('time', new Date().toTimeString());
+        coreExports.summary
+            .addHeading('Advanced Job Summary', 'h2')
+            .addImage('https://octodex.github.com/images/droidtocat.png', 'Droidtocat', {
+            width: '64',
+            height: '64'
+        })
+            .addTable([
+            [
+                { data: 'File', header: true },
+                { data: 'Result', header: true }
+            ],
+            ['foo.js', 'Pass '],
+            ['bar.js', 'Fail '],
+            ['test.js', 'Pass ']
+        ])
+            .addLink('My custom link', 'https://writeabout.net')
+            .write();
     }
     catch (error) {
         // Fail the workflow run if an error occurs
